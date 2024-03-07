b25ea89b6fa2b22690b950a90854d43622c3b8a8
LouisGuo123 <louis.guo@ucc.on.ca>
2024-03-07 13:25:04 -0500

    Fix links in /fundamentals

--- src/routes/fundamentals/+page.svelte
+++ src/routes/fundamentals/+page.svelte
@@ -1,13 +1,18 @@
+<script lang="ts">
+    import { base } from '$app/paths';
+</script>
+
+
 <p class="article-p">
     Two short articles giving primers on prerequisite knowledge for machine learning:
 </p>
 <br>
 <ul>
     <li>
-        <a href="/fundamentals/calculus/">Calculus</a>
+        <a href="{base}/fundamentals/calculus/">Calculus</a>
     </li>
     <li>
-        <a href="/fundamentals/linear-algebra/">Linear Algebra</a>
+        <a href="{base}/fundamentals/linear-algebra/">Linear Algebra</a>
     </li>
 </ul>
 
